ceabd1a35aa8d473968365644fea45f7d16c4dcd
sleijon1 <simonleijon59@gmail.com>
2019-08-16 14:46:31 +0200

    fixed resolution problem

---
+++ Assets/Scripts/Misc/ResolutionConfig.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResolutionConfig : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1260, 650, true);
+        Screen.fullScreen = !Screen.fullScreen;
+        SceneManager.LoadScene("Persistent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
